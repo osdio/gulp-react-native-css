df58b06ca0aff3efb15c41e785e134bc8dfbdfe3
soliury <lingyong1992@foxmail.com>
2015-05-11 14:40:51 +0800

    0.1.0

--- test/expected/test.js
+++ test/expected/test.js
@@ -1,45 +1,5 @@
 module.exports ={
-    "description": {
-        "fontSize": 18,
-        "textAlign": "center",
-        "color": "#ff3333",
-        "marginLeft": 2,
-        "marginRight": 2,
-        "marginTop": 2,
-        "marginBottom": 5
-    },
-    "ok": {
-        "fontSize": 18,
-        "textAlign": "center",
-        "color": "#ff3333",
-        "marginLeft": 2,
-        "marginRight": 2,
-        "marginTop": 2,
-        "marginBottom": 3,
-        "paddingLeft": 2,
-        "paddingRight": 2,
-        "paddingTop": 2,
-        "paddingBottom": 6
-    },
-    "description name": {
-        "color": "rgba(0, 0, 0, 0.2)"
-    },
-    "ok name": {
-        "color": "rgba(0, 0, 0, 0.2)"
-    },
-    "container": {
-        "paddingTop": 30,
-        "paddingBottom": 30,
-        "paddingRight": 30,
-        "paddingLeft": 30,
-        "marginTop": 65,
-        "alignItems": "center"
-    },
-    "container row": {
-        "marginBottom": 50
-    },
-    "test": {
-        "backgroundColor": "red",
-        "fontSize": 18
+    "border": {
+        "border": "1px rgba(0, 0, 0, 0.2)"
     }
 }
